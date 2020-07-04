d8caaf58472b526cb39495e6b4ab14508d94b5b7
Carlos Marcelo <carlos.marcelo@students.salle.url.edu>
2020-07-04 13:25:11 +0200

    transform and save images into mysql

--- src/Consumer/ConsumerController.php
+++ src/Consumer/ConsumerController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 namespace App\Consumer;
 
 use PhpAmqpLib\Message\AMQPMessage;
+use claviska\SimpleImage;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-final class ConsumerController
+final class ConsumerController extends AbstractController
 {
 
     public function __construct()
@@ -15,7 +17,51 @@ final class ConsumerController
 
     public function execute(AMQPMessage $msg)
     {
-        // Apply transformations
-        var_dump($msg);die();
+
+        $path = $this->getParameter('uploads_folder');
+
+        $message = json_decode($msg->body, true);
+
+        $image = new SimpleImage();
+
+        $image = $image->fromFile($path . 'original/' . $message['title']);
+
+        switch ($message['transformation']) {
+            case 'blur':
+                $image = $image->blur();
+                break;
+            case 'sepia':
+                $image = $image->sepia();
+                break;
+            case 'invert':
+                $image = $image->invert();
+                break;
+            case 'emboss':
+                $image = $image->emboss();
+                break;
+            case 'sketch':
+                $image = $image->sketch();
+                break;
+        }
+
+        $newTitle = $message['id']. '-' .$message['transformation']. '.png';
+
+        $image->toFile($path . 'transformations/' . $newTitle);
+        $mysql = new \PDO('mysql:dbname=db;host=mysql', 'user', 'password');
+
+        $sql = "INSERT INTO photos (id_photo, path, title, description, tags, original) VALUES (:id, :path, :title, :description, :tags, 0)";
+
+        $stmt =$mysql->prepare($sql);
+
+        $stmt->execute([
+            'id' => $message['id'],
+            'path' => $path . 'transformations/',
+            'title' => $newTitle,
+            'description' => $message['description'],
+            'tags' => $message['tags']
+        ]);
+        echo 'Image converted and Saved;'. PHP_EOL;
+
     }
+
 }
--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -3,16 +3,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-
 use claviska\SimpleImage;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class UploadController
 {
 
-    public function upload(Request $request)
+    public function upload(Request $request) : Response
     {
 
         $tags        = $request->get('tags');
@@ -28,33 +28,54 @@ final class UploadController
             $channel = $connection->channel();
             $channel->queue_declare($_ENV['RABBIT_QUEUE'], false, true, false, false);
 
+            $mysql = new \PDO('mysql:dbname=db;host=mysql', 'user', 'password');
 
             foreach ($files->all()['file'] as $file) {
 
+                $id = uniqid();
+                $path = 'uploads/original/';
+                $title = $id . '.png';
+
+                $image = new SimpleImage();
+
+                $image->fromFile($file->getPathName())->toFile($path . $title);
+
                 $messageBody = [];
 
-                $messageBody['file_path'] = $file->getPathname();
+                $messageBody['id'] = $id;
                 $messageBody['description'] = $description;
                 $messageBody['tags'] = $tags;
+                $messageBody['title'] = $title;
 
                 foreach ($transformations as $key => $transformation ) {
-                    $messageBody['transformation'] = $transformations;
+                    $messageBody['transformation'] = $transformation;
                     $msg = new AMQPMessage(json_encode($messageBody));
                     $channel->basic_publish($msg, $_ENV['RABBIT_EXCHANGE'], '');
                 }
+
+                $sql = "INSERT INTO photos (id_photo, path, title, description, tags, original) VALUES (:id, :path, :title, :description, :tags, 1)";
+
+                $stmt =$mysql->prepare($sql);
+
+                $stmt->execute([
+                    'id' => $id,
+                    'path' => $path,
+                    'title' => $title,
+                    'description' => $description,
+                    'tags' => $tags
+                ]);
 
             }
 
             $channel->close();
             $connection->close();
+
         }
+
+        $response = 'success';
+
+        return Response::create($response, 200);
 
     }
 
-//    private function prepareMessages($file)
-//    {
-//
-//        $image = new SimpleImage();
-//        $image->fromFile($file->getPathname())->sepia()->toScreen();
-//    }
 }
